dd0399c91ee828375772da220a1f4db8f0d130e7
Paula <pauareco@gmail.com>
2021-04-09 18:23:52 -0300

    DbSets

--- StudyUp/DataAccess/DBContext.cs
+++ StudyUp/DataAccess/DBContext.cs
@@ -10,13 +10,17 @@ namespace DataAccess
     public class DBContext : DbContext
     {
         public DbSet<User> Users { get; set; }
- 
+        public DbSet<Deck> Decks { get; set; }
+        public DbSet<Flashcard> Flashcards { get; set; }
+
         public DBContext(DbContextOptions options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasKey(u => u.Id);
-           
+            modelBuilder.Entity<Deck>().HasKey(d => d.Id);
+            modelBuilder.Entity<Flashcard>().HasKey(f => f.Id);
+
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { optionsBuilder.UseLazyLoadingProxies(); }
 
